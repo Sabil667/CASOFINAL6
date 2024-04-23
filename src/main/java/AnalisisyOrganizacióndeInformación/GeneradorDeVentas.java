3494f389cb0e7a6677e753c925d82d7a765027a1
Sabil667 <msabi@myuax.com>
2024-04-23 02:12:41 +0200

    .

--- src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
+++ src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
@@ -1,12 +1,11 @@
 package AnalisisyOrganizacióndeInformación;
 
-import AnalisisyOrganizacióndeInformación.Venta;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 public class GeneradorDeVentas {
     public static List<Venta> generarVentas(int cantidad) {
@@ -16,7 +15,8 @@ public class GeneradorDeVentas {
         for (int i = 1; i <= cantidad; i++) {
             String fecha = dateFormat.format(new Date());
             double monto = 100.0 + (500.0 - 100.0) * random.nextDouble(); // Generar un monto aleatorio entre 100.0 y 500.0
-            ventas.add(new Venta("Venta" + i, monto, fecha));
+            String nombreAleatorio = UUID.randomUUID().toString(); // Generar un nombre aleatorio
+            ventas.add(new Venta(nombreAleatorio, monto, fecha));
         }
         return ventas;
     }
