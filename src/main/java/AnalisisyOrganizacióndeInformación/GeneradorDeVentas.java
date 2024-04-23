0df5eb3c919a8b0589abe81417cf74eaaf6287c3
Sabil667 <msabi@myuax.com>
2024-04-23 03:00:16 +0200

    .

--- src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
+++ src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 public class GeneradorDeVentas {
     private static String[] nombres = {"Carlos", "Ana", "Pedro", "María", "Juan", "Carmen", "Luis", "Isabel", "Javier", "Teresa"};
@@ -12,12 +11,11 @@ public class GeneradorDeVentas {
 
     public static List<Venta> generarVentas(int cantidad) {
         List<Venta> ventas = new ArrayList<>();
-        Random random = new Random();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        for (int i = 1; i <= cantidad; i++) {
+        for (int i = 0; i < cantidad; i++) {
             String fecha = dateFormat.format(new Date());
-            double monto = 100.0 + (500.0 - 100.0) * random.nextDouble(); // Generar un monto aleatorio entre 100.0 y 500.0
-            String nombreAleatorio = nombres[random.nextInt(nombres.length)] + " " + apellidos[random.nextInt(apellidos.length)]; // Generar un nombre aleatorio
+            double monto = 100.0 + (500.0 - 100.0) * Math.random(); // Generar un monto aleatorio entre 100.0 y 500.0
+            String nombreAleatorio = nombres[i % nombres.length] + " " + apellidos[i % apellidos.length]; // Generar un nombre aleatorio
             ventas.add(new Venta(nombreAleatorio, monto, fecha));
         }
         return ventas;
