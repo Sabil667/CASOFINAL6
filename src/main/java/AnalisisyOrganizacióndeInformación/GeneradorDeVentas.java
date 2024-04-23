211bc39bc8a35d42f9653e1dfca405eeee89494b
Sabil667 <msabi@myuax.com>
2024-04-23 02:15:56 +0200

    .

--- src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
+++ src/main/java/AnalisisyOrganizacióndeInformación/GeneradorDeVentas.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 public class GeneradorDeVentas {
+    private static String[] nombres = {"Carlos", "Ana", "Pedro", "María", "Juan", "Carmen", "Luis", "Isabel", "Javier", "Teresa"};
+    private static String[] apellidos = {"García", "Rodríguez", "Pérez", "Sánchez", "López", "Martínez", "González", "Hernández", "Fernández", "Torres"};
+
     public static List<Venta> generarVentas(int cantidad) {
         List<Venta> ventas = new ArrayList<>();
         Random random = new Random();
@@ -15,7 +17,7 @@ public class GeneradorDeVentas {
         for (int i = 1; i <= cantidad; i++) {
             String fecha = dateFormat.format(new Date());
             double monto = 100.0 + (500.0 - 100.0) * random.nextDouble(); // Generar un monto aleatorio entre 100.0 y 500.0
-            String nombreAleatorio = UUID.randomUUID().toString(); // Generar un nombre aleatorio
+            String nombreAleatorio = nombres[random.nextInt(nombres.length)] + " " + apellidos[random.nextInt(apellidos.length)]; // Generar un nombre aleatorio
             ventas.add(new Venta(nombreAleatorio, monto, fecha));
         }
         return ventas;
